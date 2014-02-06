5c66e4495a2fbfd93412b0f2fa8ddd56f639bab3
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2014-02-06 11:11:25 +0100

    Fix loop.

--- ragent_api/sdk_stats.rb
+++ ragent_api/sdk_stats.rb
@@ -105,8 +105,9 @@ module SDK_STATS
     count = [0,0,0,0,0,0,0,0,0,0]
     RAGENT.user_class_subscriber.get_subscribers.each do |user_agent_class|
       arr = @daemon_stat['agents'][user_agent_class.agent_name]['process_time_specter']
+
       (0..3).each do |idxx|
-        (0..(count.size)).each do |idx|
+        (0..(count.size - 1)).each do |idx|
           count[idx] += arr[idxx][idx]
         end
       end
