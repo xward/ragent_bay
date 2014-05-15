36ef6045062c781968b96c80e1a42d770b7b2850
Julien Fouilhé <julien.fouilhe@gmail.com>
2014-05-15 17:58:33 +0200

    Start implementing collection definitions api access

--- ragent_api/ragent_api.rb
+++ ragent_api/ragent_api.rb
@@ -5,6 +5,7 @@
 
 require_relative 'incoming_messages_handler'
 require_relative 'tracking_field_mapping'
+require_relative 'collection_definition_mapping'
 require_relative 'user_agent_class'
 require_relative 'sdk_stats'
 require_relative 'rufus'
