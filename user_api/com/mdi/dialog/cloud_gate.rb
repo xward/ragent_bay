e14bffe66f38f8cf42cb0be05734d0078bf8ccef
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2014-06-02 16:58:24 +0200

    Fix unused var.

--- user_api/com/mdi/dialog/cloud_gate.rb
+++ user_api/com/mdi/dialog/cloud_gate.rb
@@ -106,7 +106,7 @@ module UserApis
         #   new_track.set_field("MDI_CC_LEGAL_SPEED", "50")
         #   user_api.mdi.dialog.cloud_gate.inject_track(new_track)
         def inject_track(track)
-          raise "Track id #{msg.id} has already been sent into the cloud. Dropping injection."  if track.id != nil
+          raise "Track id #{track.id} has already been sent into the cloud. Dropping injection."  if track.id != nil
           raise "I don't push empty track. Dropping injection." if track.fields_data.size == 0
 
           begin
