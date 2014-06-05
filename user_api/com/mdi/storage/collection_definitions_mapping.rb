7e612ae180f9b74b37dd5984eb61e1eef4955836
Julien Fouilhé <julien.fouilhe@gmail.com>
2014-06-05 17:16:21 +0200

    Add method to get definitions for an asset

--- user_api/com/mdi/storage/collection_definitions_mapping.rb
+++ user_api/com/mdi/storage/collection_definitions_mapping.rb
@@ -21,9 +21,20 @@ module UserApis
 
         # return a collection definition structs array
         def get_all()
-          RagentApi::CollectionDefinitionMapping.get_all(user_api.account)
+          @all_definitions ||= RagentApi::CollectionDefinitionMapping.get_all(user_api.account)
         end
 
+        def get_for_asset_with_type(imei, type)
+          asset_definitions = []
+          definitions = self.get_all()
+
+          definitions.each do |definition|
+            if (definition.assets == [] || definition.assets.include? imei) && definition.collects.include? type
+              asset_definitions << definition
+            end
+          end
+          asset_definitions
+        end
       end
 
     end #Storage
